794e4ca05f80e9e547654a020dbdc5342ed14104
Yong-Yeol Ahn <yongyeol@gmail.com>
2018-02-19 09:56:29 -0500

    library folder

---
+++ lib/README.md
@@ -0,0 +1,2 @@
+Create a package that you can install and use throughout the project. You can
+use tools such as [`cookiecutter`](https://github.com/audreyr/cookiecutter). 
---
+++ lib/project_package_name/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='project_package_name', 
+      version='0.1', 
+      description = 'project description', 
+      author = '...', 
+      packages = ['project_package_name'],
+)
