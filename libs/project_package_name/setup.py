d20ce54e84e5fe14febce6649f0967e5876f692d
YY Ahn <yongyeol@gmail.com>
2020-09-22 14:02:29 -0400

    clean up setup.py

--- libs/project_package_name/setup.py
+++ libs/project_package_name/setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from distutils.core import setup
+from setuptools import find_packages, setup
 
-setup(name='project_package_name', 
-      version='0.1', 
-      description = 'project description', 
-      author = '...', 
-      packages = ['project_package_name'],
+setup(
+    name="project_package_name",
+    version="0.1",
+    description="project description",
+    author="...",
+    packages=find_packages(exclude=("tests",)),
 )
