2523643da21aa7392f17c2f2b34e70d545e5062c
YY Ahn <yongyeol@gmail.com>
2020-12-28 00:04:35 -0500

    simplifying

--- install.sh
+++ install.sh
@@ -1,9 +1,2 @@
-cp -R ./data $1
-cp -R ./exps $1
-cp -R ./libs $1
-cp -R ./paper $1
-cp -R ./workflow $1
-cp ./.gitignore $1
-cp ./Makefile $1
-cp ./LICENSE $1
-touch $1/README.md
+cp -R ./* $1
+rm $1/install.sh
